ccd6728bc1c80298015ca72f73b631fcdcedca06
VIYD <viyd@i.ua>
2023-10-05 10:35:48 +0300

    v1.2

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-//https://bank.gov.ua/NBU_ovdp?date=29.08.2023&val_code=UAH&json
-
 const fs = require("node:fs");
 
 fs.readFile("data.json", (readErr, data) => {
     if (readErr == null) {
-        
         const jsondata = JSON.parse(data.toString());
 
-        fs.open("output.txt", "a", (openErr, fd) => {
+        fs.open("oldoutput.txt", "a", (openErr, fd) => {
             if (openErr != null) {
                 console.log("Error occurred while opening file:", openErr);
                 return;
             }
 
-            for (let i = 0; i < jsondata.length; i++) {
-                const jsonstring = jsondata[i].StockCode + "-" + jsondata[i].ValCode + "-" + jsondata[i].Attraction + "\n";
-                
-                fs.write(fd, jsonstring, (writeErr) => {
-                    if (writeErr != null) {
-                        console.log("Error occurred while writing file:", writeErr);
-                    }
-                });
+            function writeNext(index) {
+                if (index < jsondata.length) {
+                    const jsonstring = jsondata[index].StockCode + "-" + jsondata[index].ValCode + "-" + jsondata[index].Attraction + "\n";
+
+                    fs.write(fd, jsonstring, (writeErr) => {
+                        if (writeErr != null) {
+                            console.log("Error occurred while writing file:", writeErr);
+                        }
+                        writeNext(index + 1);
+                    });
+                } else {
+                    fs.close(fd, (closeErr) => {
+                        if (closeErr != null) {
+                            console.log("Error occurred while closing file:", closeErr);
+                        } else {
+                            console.log("File has been written!");
+                        }
+                    });
+                }
             }
 
-            fs.close(fd, (closeErr) => {
-                if (closeErr != null) {
-                    console.log("Error occurred while closing file:", closeErr);
-                } else {
-                    console.log("File has been written!");
-                }
-            });
+            writeNext(0);
         });
     } else {
         console.log("Error occurred while reading file:", readErr);
