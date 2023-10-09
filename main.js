49ce195fbd28d997f3b6c7e7a76efddca968c710
VIYD <viyd@i.ua>
2023-10-09 17:43:31 +0300

    v1.4

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 const fs = require("node:fs");
 
 fs.readFile("data.json", (readErr, data) => {
-    if (readErr == null) {
-        const jsondata = JSON.parse(data.toString());
-
-        fs.open("output.txt", "a", (openErr, fd) => {
-            if (openErr != null) {
-                console.log("Error occurred while opening file:", openErr);
-                return;
-            }
-            
-            function writeNext(index) {
-                if (index < jsondata.length) {
-                    const jsonstring = jsondata[index].StockCode + "-" + jsondata[index].ValCode + "-" + jsondata[index].Attraction + "\n";
-
-                    fs.write(fd, jsonstring, (writeErr) => {
-                        if (writeErr != null) {
-                            console.log("Error occurred while writing file:", writeErr);
-                        }
-                        writeNext(index + 1);
-                    });
-                } else {
-                    fs.close(fd, (closeErr) => {
-                        if (closeErr != null) {
-                            console.log("Error occurred while closing file:", closeErr);
-                        } else {
-                            console.log("File has been written!");
-                        }
-                    });
-                }
-            }
-
-            writeNext(0);
-        });
-    } else {
+    if (readErr != null) {
         console.log("Error occurred while reading file:", readErr);
+        return;
     }
+
+    const jsondata = JSON.parse(data.toString());
+    fs.open("output.txt", "a", (openErr, fd) => {
+        if (openErr != null) {
+            console.log("Error occurred while opening file:", openErr);
+            return;
+        }
+        
+        function writeNext(index) {
+            if (index < jsondata.length) {
+                const jsonstring = jsondata[index].StockCode + "-" + jsondata[index].ValCode + "-" + jsondata[index].Attraction + "\n";
+                fs.write(fd, jsonstring, (writeErr) => {
+                    if (writeErr != null) {
+                        console.log("Error occurred while writing file:", writeErr);
+                        return;
+                    }
+
+                    writeNext(index + 1);
+                });
+            } else {
+                fs.close(fd, (closeErr) => {
+                    if (closeErr != null) {
+                        console.log("Error occurred while closing file:", closeErr);
+                        return;
+                    } 
+                });
+            }
+        }
+        writeNext(0);
+    });
 });
